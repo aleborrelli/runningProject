725f44ac95a015d72828e48a9443b5bd113a1676
aleborrelli <ale.borrelli998@gmail.com>
2024-02-18 18:54:55 +0100

    project update: added control automatic start date and no data found management

--- main.py
+++ main.py
@@ -8,7 +8,5 @@ from tools.apiLib import ApiLib
 api_lib = ApiLib()
 token = api_lib.RequestToken()
 
-
-response = api_lib.GetActivities(token)
-
-res_df = pd.DataFrame(response.json())
+response = api_lib.GetActivities(token=token)
+res_df = pd.DataFrame(response)
--- tools/apiLib.py
+++ tools/apiLib.py
@@ -1,11 +1,16 @@
+import configparser
 import requests
-import os
 
-from tools.config import Credentials
+from datetime import datetime
+from settings.config import Credentials
+
+config = configparser.ConfigParser()
+config.read("settings/settings.txt")
 
 class ApiLib:
 
     def RequestToken(self):
+
         payload = {
             'client_id': Credentials.CLIENT_ID,
             'client_secret': Credentials.CLIENT_SECRET,
@@ -13,15 +18,43 @@ class ApiLib:
             'grant_type': Credentials.GRANT_TYPE,
             'f': 'json'
         }
-        print("Requesting Token...\n")
+        print(" > Requesting Token...\n")
         res = requests.post(Credentials.AUTH_URL, data=payload, verify=False)
         access_token = res.json()["access_token"]
-        print("Access Token = {}\n".format(access_token))
+        print("     > Access Token = {}\n".format(access_token))
 
         return access_token
 
     def GetActivities(self, token):
+
+        page_not_empty = True
         header = {'Authorization': 'Bearer ' + token}
         param = {'per_page': 200, 'page': 1}
-        response = requests.get(Credentials.ACTIVITIES_URL, headers=header, params=param)
-        return response
+        if config["DATE"]["last_download_date"] != "":
+            param["after"] = int(datetime.strptime(config["DATE"]["last_download_date"], "%Y-%m-%d").timestamp())
+
+        data_l = []
+        print(" > Starting download")
+        while page_not_empty:
+            response = requests.get(Credentials.ACTIVITIES_URL, headers=header, params=param)
+
+            if response.status_code == 200:
+
+                if len(response.json()) > 0:
+                    data_l += response.json()
+                    param["page"] += 1
+
+                else:
+                    if param["page"] > 1:
+                        print(f"     > Downloaded {param['page']} pages stopping download and updating last download date")
+                        config["DATE"]["last_download_date"] = datetime.now().strftime("%Y-%m-%d")
+                        with open("settings/settings.txt", "w") as config_file:
+                            config.write(config_file)
+                        return data_l
+                    if param["page"] == 1:
+                        print("     > No new data")
+                        return data_l
+
+            else:
+                print(f"     > API ERROR {response.status_code}")
+                return []
